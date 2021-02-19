a114bb874d9ebf8b2f9f8a1dfa86333d17ab6417
Richard <richard.daseking@gmail.com>
2021-02-19 22:47:00 +0100

    commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // index.js
-const dotenv = require('dotenv');
-dotenv.config();
-// ... client setup (keep reading)
-console.log(process.env.A);
-console.log(process.env.B);
-console.log(process.env.TOKEN);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.TOKEN);
