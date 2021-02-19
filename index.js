bc7058160b51d5987c45628c3bb91be7b4f8a9a8
Richard <richard.daseking@gmail.com>
2021-02-19 23:34:54 +0100

    commit

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-// index.js
 const dotenv = require('dotenv');
 dotenv.config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const {
+  prefix
+} = require('./config.json');
+
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 client.login(process.env.TOKEN);
 
 client.on('message', message => {
-  if (message.content === '!ping') {
-	
-	message.channel.send('Pong.');
-}
+  if (message.content === `${prefix}ping`) {
+
+    message.channel.send('Pong.');
+  } else if (message.content === `${prefix}beep`) {
+    message.channel.send('Boop.');
+  }
 });
