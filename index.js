b63949e8cc569e441cd08f77f0f1e93faf2bdbb6
Richard <richard.daseking@gmail.com>
2021-02-20 00:13:18 +0100

    c

--- index.js
+++ index.js
@@ -21,10 +21,18 @@ for (const file of commandFiles) {
 }
 
 client.on('message', message => {
-  if (message.content === `${prefix}ping`) {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    message.channel.send('Pong.');
-  } else if (message.content === `${prefix}beep`) {
-    message.channel.send('Boop.');
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) return;
+
+  try {
+  	client.commands.get(command).execute(message, args);
+  } catch (error) {
+  	console.error(error);
+  	message.reply('there was an error trying to execute that command!');
   }
+
 });
