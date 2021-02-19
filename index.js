a263d1c7a8e4f5e06f1069df92ae446f52516555
riiking <richard.daseking@gmail.com>
2021-02-19 21:57:12 +0000

    commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // index.js
+const dotenv = require('dotenv');
+dotenv.config();
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
-
 client.login(process.env.TOKEN);
