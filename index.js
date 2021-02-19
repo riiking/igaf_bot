448d5259917bd52b1a45a34814f99e838c79b67f
Richard <richard.daseking@gmail.com>
2021-02-20 00:51:49 +0100

    c

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const commandFolders = fs.readdirSync('./commands');
 for (const folder of commandFolders) {
-	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const command = require(`./commands/${folder}/${file}`);
-		client.commands.set(command.name, command);
-	}
+  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.name, command);
+  }
 }
 
 client.once('ready', () => {
@@ -30,24 +30,30 @@ for (const file of commandFiles) {
 }
 
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const commandName = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
 
   if (!client.commands.has(commandName)) return;
 
   const command = client.commands.get(commandName);
 
   if (command.args && !args.length) {
-		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-	}
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    }
+
+    return message.channel.send(reply);
+  }
 
   try {
-  	command.execute(message, args);
+    command.execute(message, args);
   } catch (error) {
-  	console.error(error);
-  	message.reply('there was an error trying to execute that command!');
+    console.error(error);
+    message.reply('there was an error trying to execute that command!');
   }
 
 });
