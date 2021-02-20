387a9f39099b8cf50944d891245c627b806a3d85
Richard <richard.daseking@gmail.com>
2021-02-20 01:01:17 +0100

    c

--- commands/basic/ping.js
+++ commands/basic/ping.js
@@ -1,6 +1,8 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
+  cooldown: 5,
+  aliases: ['pong'],
 	execute(message, args) {
 		message.channel.send('Pong.');
 	},
--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const {
 
 dotenv.config();
 client.commands = new Discord.Collection();
+
+const cooldowns = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const commandFolders = fs.readdirSync('./commands');
 for (const folder of commandFolders) {
@@ -35,10 +37,13 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
 
-  if (!client.commands.has(commandName)) return;
+  const command = client.commands.get(commandName) ||
+    client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-  const command = client.commands.get(commandName);
-
+  if (!command) return;
+  if (command.guildOnly && message.channel.type === 'dm') {
+    return message.reply('I can\'t execute that command inside DMs!');
+  }
   if (command.args && !args.length) {
     let reply = `You didn't provide any arguments, ${message.author}!`;
 
@@ -48,6 +53,26 @@ client.on('message', message => {
 
     return message.channel.send(reply);
   }
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
   try {
     command.execute(message, args);
