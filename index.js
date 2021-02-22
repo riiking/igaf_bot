375b998d2e402e4904436f3941e4e97d66d1541f
Richard <richard.daseking@gmail.om>
2021-02-22 11:24:09 +0100

    c

--- index.js
+++ index.js
@@ -29,12 +29,18 @@ const sequelize = new Sequelize('database', 'user', 'password', {
   storage: 'database.sqlite',
 });
 
-const sequelize = new Sequelize('database', 'user', 'password', {
-host: 'localhost',
-dialect: 'sqlite',
-logging: false,
-// SQLite only
-storage: 'database.sqlite',
+const Tags = sequelize.define('tags', {
+	name: {
+		type: Sequelize.STRING,
+		unique: true,
+	},
+	description: Sequelize.TEXT,
+	username: Sequelize.STRING,
+	usage_count: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0,
+		allowNull: false,
+	},
 });
 
 client.once('ready', () => {
