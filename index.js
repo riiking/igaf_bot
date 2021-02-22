edbd1499534db74435946a7ada340118f432d069
Richard <richard.daseking@gmail.om>
2021-02-22 11:28:26 +0100

    c

--- index.js
+++ index.js
@@ -30,17 +30,17 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 });
 
 const Tags = sequelize.define('tags', {
-	name: {
-		type: Sequelize.STRING,
-		unique: true,
-	},
-	description: Sequelize.TEXT,
-	username: Sequelize.STRING,
-	usage_count: {
-		type: Sequelize.INTEGER,
-		defaultValue: 0,
-		allowNull: false,
-	},
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  description: Sequelize.TEXT,
+  username: Sequelize.STRING,
+  usage_count: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+  },
 });
 
 client.once('ready', () => {
