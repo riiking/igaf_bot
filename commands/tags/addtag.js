604d054ae914ef74d91a8b933817b2262a22bd87
Richard <richard.daseking@gmail.om>
2021-02-22 12:30:50 +0100

    c

--- commands/tags/addtag.js
+++ commands/tags/addtag.js
@@ -1,5 +1,4 @@
-const Tags = require('../db/tags.js');
-
+const Tags = require('./../../db/tags.js');
 async function execute(message, args) {
   const splitArgs = args;
   const tagName = args.shift();
